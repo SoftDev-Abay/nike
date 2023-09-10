6122ea8b316c53d00966d39dd0e2a0da3b794931
Abay Aliyev <abay.aliev.03@gmail.com>
2023-09-10 21:15:44 +0600

    added nav toggle functionality

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,34 +1,73 @@
-import React from "react";
+import { React, useState } from "react";
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
 import { navLinks } from "../constants";
 
 const Nav = () => {
+  const [navVeritcalVisible, setNavVeritcalVisible] = useState(false);
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
-      <nav className="flex justify-between items-center max-container">
-        <a href="/">
-          <img src={headerLogo} alt="Logo" width={130} height={29} />
-        </a>
-        <ul
-          className="flex-1 flex justify-center 
-        items-center gap-16 max-lg:hidden"
-        >
-          {navLinks.map(({ href, label }) => (
-            <li key={label}>
+    <header className="padding-x py-8 absolute z-20 w-full max-lg:bg-white">
+      <nav className="flex justify-between items-center max-container max-lg:flex-col max-lg:items-start max-lg:gap-5">
+        <div className="flex justify-between max-lg:w-full">
+          <a href="/">
+            <img src={headerLogo} alt="Logo" width={130} height={29} />
+          </a>
+          <div className="hidden max-lg:block">
+            <img
+              className="cursor-pointer"
+              src={hamburger}
+              alt="Hamburger"
+              width={25}
+              height={25}
+              onClick={() => {
+                setNavVeritcalVisible(!navVeritcalVisible);
+              }}
+            />
+          </div>
+        </div>
+        {!navVeritcalVisible && (
+          <ul
+            className="flex-1 flex justify-center 
+        items-center gap-16  max-lg:hidden"
+          >
+            {navLinks.map(({ href, label }) => (
+              <li key={label}>
+                <a
+                  href={href}
+                  className="font-monts errat leading-normal
+                    text-lg text-slate-gray hover:border-b-coral-red hover:border-b-2"
+                >
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+        {navVeritcalVisible && (
+          <ul className="flex-1 flex max-lg:flex-col w-full">
+            {navLinks.map(({ href, label }) => (
               <a
                 href={href}
-                className="font-monts errat leading-normal
-                    text-lg text-slate-gray"
+                key={label}
+                className="hover:bg-slate-200 cursor-pointer py-3 pl-4 font-monts errat leading-normal
+            text-lg text-slate-gray "
               >
                 {label}
               </a>
-            </li>
-          ))}
-        </ul>
-        <div className="hidden max-lg:block">
-          <img src={hamburger} alt="Hamburger" width={25} height={25 } />
-        </div>
+            ))}
+          </ul>
+        )}
+        {/* <div className="hidden max-lg:block">
+          <img
+            src={hamburger}
+            alt="Hamburger"
+            width={25}
+            height={25}
+            onClick={() => {
+              setNavVeritcalVisible(!navVeritcalVisible);
+            }}
+          />
+        </div> */}
       </nav>
     </header>
   );
