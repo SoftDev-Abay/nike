a54c6d3fed511da7fcce65e8f66343f51edb0215
Abay Aliyev <abay.aliev.03@gmail.com>
2023-09-10 15:26:30 +0600

    finished v

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularComponents from "./PopularComponents";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularComponents,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
